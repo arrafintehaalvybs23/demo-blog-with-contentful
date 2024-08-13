042b64722c9da5dfd6c983d1973f1b0465eab89d
Maruf1075 <marufcu98@gmail.com>
2024-08-13 14:51:17 +0600

    [FIX]: Configured Header Data with Contentful

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,44 @@
 "use client";
 
-import Link from "next/link";
 import { useState, useRef, useEffect } from "react";
+import Link from "next/link";
+import { createClient } from "contentful";
+
+console.log("Space ID:", process.env.CONTENTFUL_SPACE_ID);
+console.log("Access Token:", process.env.CONTENTFUL_ACCESS_TOKEN);
+
+// Set up Contentful client
+// const client = createClient({
+//   space: process.env.CONTENTFUL_SPACE_ID,
+//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+// });
+
+const client = createClient({
+  space: "b5o1vh7kxenn",
+  accessToken: "6muMAQT7MxHtCFMEj802LYKO2rV7OGvZYuXwPmwgAkI",
+});
 
 export function Header() {
   const [openDropdown, setOpenDropdown] = useState(null);
+  const [headerData, setHeaderData] = useState(null);
   const dropdownRefs = useRef({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.getEntries({
+          content_type: "header",
+          include: 2,
+        });
+
+        setHeaderData(response?.items[0]?.fields);
+      } catch (error) {
+        console.error("Error fetching header data from Contentful:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleClickOutside = (event) => {
     if (
@@ -19,7 +52,6 @@ export function Header() {
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
-
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
@@ -29,203 +61,73 @@ export function Header() {
     setOpenDropdown(openDropdown === id ? null : id);
   };
 
+  if (!headerData) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <header className="bg-gray-900 text-white w-full py-10 px-4 mb-10">
+    <header className="bg-gray-900 text-white w-full py-6 px-4 mb-10">
       <div className="container mx-auto flex justify-between items-center px-5">
         {/* Logo */}
-        <Link href="/" className="text-5xl font-bold flex items-center">
-          Contentful Demo
+        <Link href="/" className="text-4xl font-bold flex items-center">
+          <img
+            src={headerData.icon.fields.file.url}
+            alt={headerData.icon.fields.title}
+            className="h-12 mr-4"
+          />
+          {headerData.title}
         </Link>
 
         {/* Navigation */}
-        <nav className="relative">
-          {/* Home with Dropdown */}
-          <div
-            className="inline-block relative"
-            ref={(el) => (dropdownRefs.current.home = el)}
-          >
-            <button
-              onClick={() => handleDropdownToggle('home')}
-              className="px-3 hover:underline flex items-center"
+        <nav className="relative flex flex-wrap items-center justify-center">
+          {headerData.navigationButtons.map((navItem, index) => (
+            <div
+              key={index}
+              className="inline-block relative"
+              ref={(el) => (dropdownRefs.current[navItem.fields.title] = el)}
             >
-              Home
-              <svg
-                className="w-4 h-4 ml-1"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+              <button
+                onClick={() => handleDropdownToggle(navItem.fields.title)}
+                className="px-3 hover:underline flex items-center"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-            {openDropdown === 'home' && (
-              <ul className="absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg z-20">
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
+                {navItem.fields.title}
+                {navItem.fields.subNavigation && (
+                  <svg
+                    className="w-4 h-4 ml-1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                   >
-                    Home 1
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Home 2
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Home 3
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </div>
-
-          {/* Posts with Dropdown */}
-          <div
-            className="inline-block relative"
-            ref={(el) => (dropdownRefs.current.posts = el)}
-          >
-            <button
-              onClick={() => handleDropdownToggle('posts')}
-              className="px-4 hover:underline flex items-center"
-            >
-              Posts
-              <svg
-                className="w-4 h-4 ml-1"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-            {openDropdown === 'posts' && (
-              <ul className="absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg z-20">
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Post 1
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Post 2
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Post 3
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </div>
-
-          {/* Other Links */}
-          <Link href="/" className="px-4 hover:underline">
-            Fashion
-          </Link>
-          <Link href="/" className="px-4 hover:underline">
-            Lifestyle
-          </Link>
-          <Link href="/" className="px-4 hover:underline">
-            Technology
-          </Link>
-          <Link href="/" className="px-4 hover:underline">
-            Sports
-          </Link>
-
-          {/* Pages with Dropdown */}
-          <div
-            className="inline-block relative"
-            ref={(el) => (dropdownRefs.current.pages = el)}
-          >
-            <button
-              onClick={() => handleDropdownToggle('pages')}
-              className="px-3 hover:underline flex items-center"
-            >
-              Pages
-              <svg
-                className="w-4 h-4 ml-1"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-            {openDropdown === 'pages' && (
-              <ul className="absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg z-20">
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Page 1
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Page 2
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 hover:bg-gray-200"
-                    onClick={() => setOpenDropdown(null)}
-                  >
-                    Page 3
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </div>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                )}
+              </button>
+              {openDropdown === navItem.fields.title &&
+                navItem.fields.subNavigation && (
+                  <ul className="absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg z-20">
+                    {navItem.fields.subNavigation.map(
+                      (subNavItem, subIndex) => (
+                        <li key={subIndex}>
+                          <Link
+                            href={subNavItem.fields.link}
+                            className="block px-4 py-2 hover:bg-gray-200"
+                            onClick={() => setOpenDropdown(null)}
+                          >
+                            {subNavItem.fields.title}
+                          </Link>
+                        </li>
+                      )
+                    )}
+                  </ul>
+                )}
+            </div>
+          ))}
         </nav>
       </div>
     </header>
